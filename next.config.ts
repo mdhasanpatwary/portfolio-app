a3bd456f5e49c1a010c9e9220684648b099a973c
MD Hasan Patwary <patwary.web@gmail.com>
2025-08-26 17:10:03 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -13,9 +13,9 @@ const nextConfig: NextConfig = {
         // pathname: '/**',
       },
     ],
-    formats: ["image/webp", "image/avif"],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 480, 640],
+    deviceSizes: [640, 768, 1024, 1280, 1536],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512],
+    formats: ["image/avif", "image/webp"],
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
